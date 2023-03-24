4b7285ac1f5f514dbd1e4d3efb8725f204445cd2
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-03-24 17:18:56 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function is to calculate a person's salary
+ */
+function calculate() {
+  
+  const taxRate = 0.18
+
+  //input 
+  const hours = parseInt(document.getElementById('hours').value)
+  const pay = parseInt(document.getElementById('rate').value)
+
+  //process
+  const salary = hours * pay
+  const tax = salary * taxRate
+  const takehome = salary * (1.0 - taxRate)
+
+  //output
+  document.getElementById('take-home').innerHTML = 'Your pay will be: $' + takehome.toFixed(2)
+  document.getElementById('government').innerHTML = 'The government takes: $' + tax.toFixed(2)
+}
